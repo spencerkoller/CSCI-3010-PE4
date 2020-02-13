acf1f02f2a510f9dc363adc7dbdcd4da50b95632
tannerball <tanner.ball@colorado.edu>
2020-02-12 22:28:06 -0800

    tanner displayboard()

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -18,19 +18,31 @@ std::string * CreateBoard(){
     return boardArr;
 }
 
-
-void DisplayBoard(std::string * arr){
+void DisplayBoard(std::string * arr){ // Tanner
     std::cout << "Tic-Tac-Toe" << std::endl;
-    std::cout << "     |     |     " << std::endl;
-    std::cout << arr[0]<<"    | " << arr[1] << "    |  " << arr[2] << std::endl;
-    std::cout << "_____|_____|_____" << std::endl;
-    std::cout << "     |     |     " << std::endl;
-    std::cout << arr[3] << "     | " << arr[4] << "    |  " << arr[5] << std::endl;
-    std::cout << "_____|_____|_____" << std::endl;
-    std::cout << "     |     |     " << std::endl;
-    std::cout << arr[6] << "     | " << arr[7] << "    |  " << arr[8] << std::endl;
-    std::cout << "     |     |     " << std::endl;
+    std::cout << "     |     |   " << std::endl;
+    std::cout << arr[0] << "    |" << arr[1] << "     |" << arr[2] << std::endl;
+    std::cout << "-----|-----|-----" << std::endl;
+    std::cout << arr[3] << "     |" << arr[4] << "     |" << arr[5] << std::endl;
+    std::cout << "-----|-----|-----" << std::endl;
+    std::cout << arr[6] << "     |" << arr[7] << "     |" << arr[8] << std::endl;
+    std::cout << "     |     |   " << std::endl;
 }
+
+
+// void DisplayBoard(std::string * arr){ // Spencer
+//     std::cout << "Tic-Tac-Toe" << std::endl;
+//     std::cout << "     |     |     " << std::endl;
+//     std::cout << arr[0]<<"    | " << arr[1] << "    |  " << arr[2] << std::endl;
+//     std::cout << "_____|_____|_____" << std::endl;
+//     std::cout << "     |     |     " << std::endl;
+//     std::cout << arr[3] << "     | " << arr[4] << "    |  " << arr[5] << std::endl;
+//     std::cout << "_____|_____|_____" << std::endl;
+//     std::cout << "     |     |     " << std::endl;
+//     std::cout << arr[6] << "     | " << arr[7] << "    |  " << arr[8] << std::endl;
+//     std::cout << "     |     |     " << std::endl;
+// }
+
 int main(){
     std::string * board = CreateBoard();
     DisplayBoard(board);
